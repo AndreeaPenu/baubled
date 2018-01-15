3bcbb69ff53f7d0fe4c680f0bf668ff97e80190c
AndreeaPenu <andreea_penu@hotmail.Com>
2018-01-15 22:01:02 +0100

    add effect picker & on/off switch

--- baubled/app/components/MyLeds/MyLeds.js
+++ baubled/app/components/MyLeds/MyLeds.js
@@ -1,9 +1,9 @@
 import React, {Component} from 'react';
-import { AppRegistry, Text, View, StyleSheet, Dimensions, ListView, TouchableHighlight, Button, AsyncStorage, Image} from 'react-native';
+import { AppRegistry, Text, View, StyleSheet, Dimensions, ListView, TouchableHighlight, Button, AsyncStorage, Image, Picker, Switch} from 'react-native';
 import { ColorPicker } from 'react-native-color-picker';
 
 
-const Picker = () => (
+const PickerColor = () => (
     <ColorPicker
       onColorSelected={color => alert(`Color selected: ${color}`)}
       style={{flex: 1}}
@@ -11,33 +11,23 @@ const Picker = () => (
   )
 
 export default class MyLeds extends React.Component {
-/*     static navigationOptions = {
+/*      static navigationOptions = {
         title: 'Bauble 1',
-    }; */
+    };  */
     constructor(){
         super();
         this.state = {
-
+            switchValue:false,
+            effect: 'geen'
         }
     }
 
-    
-   
-    onPress = () => {
-        let image =  <Image
-            style={styles.onImage}
-            source={require('./on-groen.png')}
-        />;
-        return(
-            <TouchableHighlight
-            onPress={this.onPress}
-            >
-                {image}
-            </TouchableHighlight>
-
-        )
-        
+    onSwitchChange(value){
+        this.setState({
+            switchValue:value
+        })
     }
+    
 
     render() {
         return (
@@ -45,21 +35,33 @@ export default class MyLeds extends React.Component {
                 <View style={styles.row}>
                     
                         <View style={styles.box}>
-                        <TouchableHighlight
-                        onPress={this.onPress}
-                        >
-                            <Image
-                                style={styles.onImage}
-                                source={require('./on-wit.png')}
+                            <Switch
+                                value={this.state.switchValue}
+                                onValueChange={(value)=>this.onSwitchChange(value)}
                             />
-                        </TouchableHighlight>
                         </View>
                 
                     <View style={styles.box}>
-                        <Picker />
+                        <PickerColor />
                     </View>
                 </View>
-                
+
+                <View style={styles.row}>
+                    <View style={styles.box}>
+                        <Picker
+                            selectedValue={this.state.effect}
+                            onValueChange={(itemValue, itemIndex) => this.setState({effect: itemValue})}>
+                            <Picker.Item label="Geen effect" value="geen" />
+                            <Picker.Item label="Flikkeren" value="flikkeren" />
+                            <Picker.Item label="Traag Flikkeren" value="traag-flikkeren" />
+                            <Picker.Item label="Regenboog" value="regenboog" />
+                            <Picker.Item label="2 Kleuren" value="2kleuren" />
+                        </Picker>
+                    </View>
+                </View>
+                <View style={styles.box}>
+                    
+                </View>
             </View>
         )
     }
