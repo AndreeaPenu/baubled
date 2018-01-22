089539024ac1d09634fea672538ce497a5860d26
Adriaan Vermeire <adriaan.vermeire@student.kdg.be>
2018-01-22 14:48:31 +0100

    Add php file for forms posts

---
+++ baubled-site/control.php
@@ -0,0 +1,16 @@
+<?php
+
+  if($_POST['checkbox'])
+?>
+
+<?php echo $_POST['checkbox'] ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Control</title>
+  </head>
+  <body>
+
+  </body>
+</html>
