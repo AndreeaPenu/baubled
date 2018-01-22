0ed8834cc7aee82a0afde839ad210b8b652c190d
Adriaan Vermeire <adriaan.vermeire@student.kdg.be>
2018-01-22 15:07:43 +0100

    bug fix maybe

--- baubled-site/control.php
+++ baubled-site/control.php
@@ -5,9 +5,8 @@
     <title>Control</title>
   </head>
   <body>
-    <?php $c =  $_POST['checkbox'] ?>
-    <?php if(isset($c)): ?>
-      <p>Set: <?= $c ?></p>
+    <?php if(isset($_POST['checkbox'])): ?>
+      <p>Set: <?= $_POST['checkbox']  ?></p>
     <?php endif ?>
   </body>
 </html>
